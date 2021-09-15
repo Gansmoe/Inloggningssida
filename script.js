a3d29c1079994ada06171255053622650f94ade6
Isac Gansmoe <isac@gansmoe.se>
2021-09-15 22:47:28 +0200

    Ändrade alla .innerText och kommenterade koden

--- script.js
+++ script.js
@@ -3,24 +3,23 @@ const body = document.getElementById("body");
 
 
 function loginPage() {
-    let createDiv = document.createElement("div");
-    createDiv.id = "div";
-    body.appendChild(createDiv);
+    createDiv();
     const div = document.getElementById("div");
 
-    let user = document.createElement("input");
+    const user = document.createElement("input");
     user.type = "text";
     user.id = "user";
     div.appendChild(user);
 
-    let psw = document.createElement("input");
+    const psw = document.createElement("input");
     psw.type = "text";
     psw.id = "psw";
     div.appendChild(psw);
 
-    let btn = document.createElement("button");
+    const btn = document.createElement("button");
     btn.id = "inBtn";
-    btn.innerHTML = "Logga in";
+    const text = document.createTextNode("Logga in");
+    btn.appendChild(text);
     div.appendChild(btn);
 
     const inBtn = document.getElementById("inBtn");
@@ -33,13 +32,15 @@ function loginPage() {
         logIn(user, psw);
 
 
-    });
-};
+    })
+}
+// Kod för att generera inloggningssidan och göra inloggningsförsök
 
 function logOutReset() {
     localStorage.removeItem("username");
     loginPage();
 }
+// Kod för att logga ut från sidan och ta bort användarnamnet ur localstorage
 
 function checkUser() {
     const user = localStorage.getItem("username");
@@ -51,64 +52,68 @@ function checkUser() {
     else {
         loginPage();
     }
-};
+}
+// Kontroll om användaren är inloggad redan, är den inte det skickas den till inloggningssidan
 
 function logIn(user, psw) {
     if (user == "test" && psw == "1234") {
 
-        let createDiv = document.createElement("div");
-        createDiv.id = "div";
-        body.appendChild(createDiv);
+        createDiv();
         const div = document.getElementById("div");
 
-        let myText = document.createElement("p");
-        myText.innerText = "Du är inloggad";
+        const myText = document.createElement("p");
+        const text = document.createTextNode("Du är inloggad");
+        myText.appendChild(text);
         div.appendChild(myText);
 
-        let logOut = document.createElement("button");
+        const logOut = document.createElement("button");
         logOut.id = "logOut";
-        logOut.innerText = "Logga ut";
+        const btnText = document.createTextNode("Logga ut");
+        logOut.appendChild(btnText);
         div.appendChild(logOut);
 
         localStorage.setItem("username", user);
 
-        let logOutBtn = document.getElementById("logOut");
+        const logOutBtn = document.getElementById("logOut");
 
         logOutBtn.addEventListener("click", function () {
             deleteDiv();
             logOutReset();
-        })
+        }) // Genererar en sida vid lyckad inloggning
 
     }
     else {
-        let createDiv = document.createElement("div");
-        createDiv.id = "div";
-        body.appendChild(createDiv);
-        const div = document.getElementById("div")
+
+        createDiv();
+        const div = document.getElementById("div");
 
         const failedText = document.createElement("p");
-        failedText.innerText = "Fel lösenord eller användarnamn, vänligen försök igen"
+        const text = document.createTextNode("Fel lösenord eller användarnamn, vänligen försök igen");
+        failedText.appendChild(text);
         div.appendChild(failedText);
+
         const backBtn = document.createElement("button");
-        backBtn.innerText = "Tillbaka";
+        const btnText = document.createTextNode("Tillbaka");
+        backBtn.appendChild(btnText); 
         div.appendChild(backBtn);
+
         backBtn.addEventListener("click", function () {
             deleteDiv();
             loginPage();
 
-        });
+        })// Genererar en sida för misslyckad inloggning
     }
 };
 
 function deleteDiv() {
     const divDelete = document.getElementById("div");
     divDelete.remove();
-}
+} // Funktion för att ta bort HTML
 
 function createDiv() {
     let createDiv = document.createElement("div");
     createDiv.id = "div";
     body.appendChild(createDiv);
-}
+} // Funktion för att lägga till en div
 
 checkUser();
